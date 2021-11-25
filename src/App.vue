ddf6c436c501a35b83987f0a0622d64a645bb7e2
Miguelrc9991 <miguel.ricocorrales@gmail.com>
2021-11-25 09:25:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Galeria/>
+  </div>
+</template>
+
+<script>
+import Galeria from './components/Galeria.vue'
+export default {
+  name: 'App',
+  components: {
+    Galeria
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Galeria.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container mt-5">
+        <h1 class="display-1">Reproductor Video</h1>
+        <div id="yt" align="center">
+            
+            <label for="idvideo">
+                Pega aquí la ID del video que quieras mostrar:
+            </label>
+            <input type="text" id="idvideo" v-model="video" placeholder="Ejemplo: ZEFrbP7wYZU" value="ZEFrbP7wYZU" class="form-control input-lg">
+          <hr>
+          <iframe v-bind:src="url + video"
+            v-bind:class="responsive"
+             v-bind:width="ancho"  
+              v-bind:height="alto"
+               v-bind:frameborder="borde"
+                v-bind:allowFullscreen="pcompleta"
+
+            >
+            </iframe>
+            <p class="txt_videoactual">
+                Se está reproduciendo : <kbd> {{this.video}}</kbd>
+            </p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Galeria',
+  props: {
+    
+  },
+  data(){
+      return{
+
+            responsive: "embed-responsive-item",
+      ancho: "100%",
+      alto : "500",
+      url: "https://www.youtube.com/embed/",
+      video:"ZEFrbP7wYZU",
+      borde : 0,
+      pccompleta:null 
+      }
+    
+  }
+}
+</script>
+<style scoped>
+
+</style>
